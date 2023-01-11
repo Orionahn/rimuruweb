c8b2c8c323a8f7037ba79530262bcdaa179df03c
Pedro G <pdghodsi@gmail.com>
2023-01-11 04:53:47 -0700

    Documentation on Database

--- config/database.php
+++ config/database.php
@@ -1,16 +1,15 @@
 <?php
-define("DB_SERVER", "localhost");
-define("DB_USER", "u371728628_rimuru");
-define("DB_PASSWORD", "1>B#tu+%qGgMU/Op");
-define("DB_DATABASE", "u371728628_rimuru_dev");
-
+define("DB_SERVER", "redacted");
+define("DB_USER", "redacted");
+define("DB_PASSWORD", "redacted");
+define("DB_DATABASE", "redacted");
+// Database connections, will differ depending on localhost/remote host
 
 
 
 //Create Connection to Database using MySQLi
 $connect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
 //Check the connection
-
 if ($connect->connect_error) {
     die('Connection to Database failed ' . $connect->connect_error);
 }
