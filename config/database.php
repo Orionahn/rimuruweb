5cecdc521146ba18fdd8193fa7179924982674cb
Orionahn <101444254+Orionahn@users.noreply.github.com>
2023-01-11 03:24:43 -0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_SERVER", "localhost");
-define("DB_USER", "id20132697_pedro");
+define("DB_USER", "u371728628_rimuru");
 define("DB_PASSWORD", "1>B#tu+%qGgMU/Op");
-define("DB_DATABASE", "id20132697_rimuru_dev");
+define("DB_DATABASE", "u371728628_rimuru_dev");
 
 
 
